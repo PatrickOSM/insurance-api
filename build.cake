d3e71117d1d44eb1a8c8e4b0551fbbfeef4b1374
Patrick Moreno <patrickosm@icloud.com>
2022-02-28 21:57:00 -0300

    Create database mock, unit test structure and cake
    - Create a mock database with some data for testing purpose;
    - Add cake build for future CI integration;
    -  Updated .Net API from 6.0.1 to 6.0.2;

---
+++ build.cake
@@ -0,0 +1,47 @@
+﻿#addin nuget:?package=Cake.Coverlet&version=2.5.4
+
+var target = Argument("target", "Test");
+var configuration = Argument("configuration", "Debug");
+
+//////////////////////////////////////////////////////////////////////
+// TASKS
+//////////////////////////////////////////////////////////////////////
+
+
+Task("Build")
+    .Does(() =>
+{
+    DotNetCoreBuild("Insurance.API.sln", new DotNetCoreBuildSettings
+    {
+        Configuration = configuration,
+    });
+});
+
+Task("Test")
+    .IsDependentOn("Build")
+    .Does(() =>
+{
+	var coverletSettings = new CoverletSettings {
+        CollectCoverage = true,
+        CoverletOutputFormat = CoverletOutputFormat.opencover | CoverletOutputFormat.json,
+        MergeWithFile = MakeAbsolute(new DirectoryPath("./coverage.json")).FullPath,
+        CoverletOutputDirectory = MakeAbsolute(new DirectoryPath(@"./coverage")).FullPath
+    };
+	
+	Coverlet(
+        "./tests/Insurance.Api.IntegrationTests/bin/Debug/net6.0/Insurance.Api.IntegrationTests.dll", 
+        "./tests/Insurance.Api.IntegrationTests/Insurance.Api.IntegrationTests.csproj", 
+        coverletSettings);
+		
+	Coverlet(
+        "./tests/Insurance.Api.UnitTests/bin/Debug/net6.0/Insurance.Api.UnitTests.dll", 
+        "./tests/Insurance.Api.UnitTests/Insurance.Api.UnitTests.csproj", 
+        coverletSettings);
+	
+});
+
+//////////////////////////////////////////////////////////////////////
+// EXECUTION
+//////////////////////////////////////////////////////////////////////
+
+RunTarget(target);
---
+++ src/Insurance.Api.Infrastructure/Migrations/20220301004538_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Insurance.Api.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InsurancePolicies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DriversLicence = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EffectiveDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Premium = table.Column<decimal>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InsurancePolicies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(254)", maxLength: 254, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Password", "Role" },
+                values: new object[] { new Guid("6648c89f-e894-42bb-94f0-8fd1059c86b4"), "user@email.com", "$2a$11$QPgeTHhKzieM5ihVjRXZ2OokicJBaQRp/rhQrwprEESY50O4hDiXq", "User" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Password", "Role" },
+                values: new object[] { new Guid("687d9fd5-2752-4a96-93d5-0f33a49913c6"), "admin@email.com", "$2a$11$wzCykLNtQ8JRTDPDpP57deaWfuZIxzpygcwtKg3peP2iQVZ86LuJ2", "Admin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InsurancePolicies");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
