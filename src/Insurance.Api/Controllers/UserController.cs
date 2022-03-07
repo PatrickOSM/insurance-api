cbbc67e3344af695697e381565bc994035e1fa16
Patrick Moreno <patrickosm@icloud.com>
2022-03-07 20:36:41 -0300

    Do a cleanup in the user controller
    - Remove some controller code that I don't want to use anymore in this API;
    - Remove some services that is not needed anymore;
    - Simplifying this API code;

--- src/Insurance.Api/Controllers/UserController.cs
+++ src/Insurance.Api/Controllers/UserController.cs
@@ -1,15 +1,10 @@
-﻿using Insurance.Api.Application.DTOs;
-using Insurance.Api.Application.DTOs.Auth;
+﻿using Insurance.Api.Application.DTOs.Auth;
 using Insurance.Api.Application.DTOs.User;
-using Insurance.Api.Application.Filters;
 using Insurance.Api.Application.Interfaces;
-using Insurance.Api.Domain.Auth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Threading.Tasks;
-using ISession = Insurance.Api.Domain.Auth.Interfaces.ISession;
 
 namespace Insurance.Api.Controllers
 {
@@ -20,13 +15,11 @@ namespace Insurance.Api.Controllers
     {
         private readonly IUserService _userService;
         private readonly IAuthService _authService;
-        private readonly ISession _session;
 
-        public UserController(IUserService userService, IAuthService authService, ISession session)
+        public UserController(IUserService userService, IAuthService authService)
         {
             _userService = userService;
             _authService = authService;
-            _session = session;
         }
 
         /// <summary>
@@ -47,73 +40,6 @@ namespace Insurance.Api.Controllers
                 return BadRequest(new { message = "Username or password is incorrect" });
             }
             return Ok(_authService.GenerateToken(user));
-        }
-
-
-        /// <summary>
-        /// Returns all users in the database
-        /// </summary>
-        /// <param name="filter"></param>
-        /// <returns></returns>
-        [ProducesResponseType(typeof(PaginatedList<GetUserDto>), StatusCodes.Status200OK)]
-        [Authorize(Roles = Roles.Admin)]
-        [HttpGet]
-        public async Task<ActionResult<PaginatedList<GetUserDto>>> GetUsers([FromQuery] GetUsersFilter filter)
-        {
-            return Ok(await _userService.GetAllUsers(filter));
-        }
-
-
-        /// <summary>
-        /// Get one user by id from the database
-        /// </summary>
-        /// <param name="id">The user's ID</param>
-        /// <returns></returns>
-        [Authorize(Roles = Roles.Admin)]
-        [HttpGet]
-        [Route("{id}")]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(GetUserDto), StatusCodes.Status200OK)]
-        public async Task<ActionResult<GetUserDto>> GetUserById(Guid id)
-        {
-            GetUserDto user = await _userService.GetUserById(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(user);
-        }
-
-        [Authorize(Roles = Roles.Admin)]
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<ActionResult<GetUserDto>> CreateUser(CreateUserDto dto)
-        {
-            GetUserDto newAccount = await _userService.CreateUser(dto);
-            return CreatedAtAction(nameof(GetUserById), new { id = newAccount.Id }, newAccount);
-        }
-
-        [HttpPatch("updatePassword")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<ActionResult> UpdatePassword([FromBody] UpdatePasswordDto dto)
-        {
-            _ = await _userService.UpdatePassword(_session.UserId, dto);
-            return NoContent();
-        }
-
-        [Authorize(Roles = Roles.Admin)]
-        [HttpDelete("{id:guid}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> DeleteUser(Guid id)
-        {
-            bool deleted = await _userService.DeleteUser(id);
-            if (deleted)
-            {
-                return NoContent();
-            }
-
-            return NotFound();
         }
     }
 }
